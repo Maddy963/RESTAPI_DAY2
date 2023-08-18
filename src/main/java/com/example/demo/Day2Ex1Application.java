66da53cec0ca6378c73efbe2c87cd2441a8f5191
Maddy963 <madhavan090603@gmail.com>
2023-08-18 15:56:20 +0530

    first commit

---
+++ src/main/java/com/example/demo/Day2Ex1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2Ex1Application.class, args);
+	}
+
+}
